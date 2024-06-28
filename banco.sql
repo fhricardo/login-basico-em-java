d63c83b915a02a7caa41e52abd6c92f4359f3354
Flavio Ricardo <fhricardo@gmail.com>
2024-06-28 09:34:00 -0300

    Arquivo de criação da tabela no banco

---
+++ banco.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `users` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
